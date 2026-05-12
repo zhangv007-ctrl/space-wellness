0e76b332e1008830109ffaaa42a80200c01c6ae6
Wei Zhang <@gmail.com>
2026-05-12 15:47:30 -0400

    fix: middleware pass pathname and UA headers

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,13 @@
-export function middleware() {}
-export const config = { matcher: [] }
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+  response.headers.set('x-pathname', request.nextUrl.pathname)
+  response.headers.set('x-ua', request.headers.get('user-agent') || '')
+  return response
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
